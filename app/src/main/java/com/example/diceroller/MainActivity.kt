d81e59d5d6695e4b2ea771e7180e82ab675602c5
DarlynCaicedo <darlyn.caicedo.calderon@gmail.com>
2022-04-07 16:38:34 -0500

    Desarrollo del Coding challenge

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resulttext : TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //La clase R se genera cuando se compila la APP - incluye los activos de la APP
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.btn_roll)
+        rollButton.setOnClickListener{ rollDice() }
+
+        resulttext = findViewById(R.id.tv_result)
+
+        val countButton : Button = findViewById(R.id.btn_count)
+        countButton.setOnClickListener{ countUp()}
+
     }
+
+    private fun rollDice() {
+        //Toast.makeText(this, "Button clicked",Toast.LENGTH_SHORT).show()
+        resulttext.text = (1..6).random().toString()
+    }
+
+    private fun countUp() {
+
+        with(resulttext){
+            when(this.text){
+                "Hello World!" -> this.text = 1.toString()
+                "6" -> this.text
+                else -> this.text = (this.text.toString().toInt() + 1).toString()
+            }
+        }
+
+
+    }
+
+
 }
